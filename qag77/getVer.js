c353c6b91e8424c70b616ee2960d65a3178931bd
Kris <159937293+Gamerboss3094@users.noreply.github.com>
2024-03-18 12:47:19 -0700

    Create getVer.js

---
+++ qag77/getVer.js
@@ -0,0 +1,28 @@
+var LVinfo = {
+  "version": [
+    {
+      "major": 1,
+      "minor": 1,
+      "patch": 0,
+      "full": "1.1.0"
+    }
+  ]
+};
+var CV = CVinfo.version[0];
+var LV = LVinfo.version[0];
+
+function updateAvailable() {
+  if(LV.major > CV.major) {
+    return true;
+  } else {
+    if(LV.minor > CV.minor) {
+      return true;
+    } else {
+      if(LV.patch > CV.patch) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
