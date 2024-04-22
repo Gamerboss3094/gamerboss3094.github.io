872dc66f795558e93ddb548e516ed83bb7e4090e
Kris <159937293+Gamerboss3094@users.noreply.github.com>
2024-04-22 10:41:19 -0700

    Update updater.js

--- qag77/updater.js
+++ qag77/updater.js
@@ -1,36 +1,7 @@
-var LVinfo = {
-  "version": [
-    {
-      "major": 999,
-      "minor": 999,
-      "patch": 999,
-      "full": 'Cannot show latest version, click "Update" and it will automatically download the latest version'
-    }
-  ]
-};
-var CV = CVinfo.version[0];
-var LV = LVinfo.version[0];
-
-function updateAvailable() {
-  if(LV.major > CV.major) {
-    return true;
-  } else {
-    if(LV.minor > CV.minor) {
-      return true;
-    } else {
-      if(LV.patch > CV.patch) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-}
-
 function updatePrompt() {
   swal({
     title: 'Update Available!',
-    text: 'Newest Version: ' + LV.full + '\n Current Version: ' + CV.full,
+    text: 'Cannot show latest version, click "Update" to automatically download the latest version',
     icon: 'warning',
     buttons: {
       cancel: 'Close',
@@ -48,9 +19,7 @@ function updatePrompt() {
 
 
 function update() {
-  if(updateAvailable()) {
-    updatePrompt();
-  }
+  updatePrompt();
 }
 
 update();
