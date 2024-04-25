5a3a577673e27ada033e324d0b3ce38bc06ec1dd
Kris <159937293+Gamerboss3094@users.noreply.github.com>
2024-04-25 12:41:42 -0700

    Update getVer.js

--- qag77/getVer.js
+++ qag77/getVer.js
@@ -15,9 +15,15 @@ function updateAvailable() {
   if(LV.major > CV.major) {
     return true;
   } else {
+    if(CV.major > LV.major) {
+      return false;
+    }
     if(LV.minor > CV.minor) {
       return true;
     } else {
+      if(CV.minor > LV.minor) {
+        return false;
+      }
       if(LV.patch > CV.patch) {
         return true;
       } else {
