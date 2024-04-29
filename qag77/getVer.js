e65a992378fdeb481e12cdaaa4a5dee12568fef2
Kris <159937293+Gamerboss3094@users.noreply.github.com>
2024-04-29 08:45:58 -0700

    Update getVer.js

--- qag77/getVer.js
+++ qag77/getVer.js
@@ -20,7 +20,9 @@ var CV = CVinfo.version[0];
 var LV = LVinfo.version[0];
 
 function updateAvailable() {
-  alert('Just ignore the following messages, i am trying to update the updating system');
-  alert(CVinfo.full);
-  alert(verOrder.indexOf(CVinfo.full));
+  if(verOrder.indexOf(CV.full) < (verOrder.length - 1)) {
+    return true;
+  } else {
+    return false;
+  }
 }
